2063278695d14e312384c444fd09a28464053840
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-22 12:27:50 +0530

    Added:-to add win or loss

--- gambler.sh
+++ gambler.sh
@@ -3,3 +3,16 @@ echo "welcome to gambler"
 
 STAKE=100
 BET=1
+
+function bet(){
+result=$((RANDOM%2))
+if [ $result -eq 1 ]
+then
+	cash+=$BET
+	echo "win"
+else
+	cash-=$BET
+	echo "loss"
+fi
+}
+bet
