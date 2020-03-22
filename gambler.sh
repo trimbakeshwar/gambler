6eccdcd2ac40078aba60a92e16f13766db2c2de6
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-22 13:14:15 +0530

    Added:-calculate caash ein or loss and leave for the day when 50% loss or wwin

--- gambler.sh
+++ gambler.sh
@@ -3,16 +3,25 @@ echo "welcome to gambler"
 
 STAKE=100
 BET=1
-
+PERCENT=50
+WIN=150
+LOSS=50
+read -p "enter cash" cash
 function bet(){
 result=$((RANDOM%2))
 if [ $result -eq 1 ]
 then
-	cash+=$BET
-	echo "win"
+	$((cash+=$BET))
+	echo $cash
 else
-	cash-=$BET
-	echo "loss"
+	$((cash-=$BET))
+	echo $cash
 fi
 }
 bet
+function playing(){
+while [[ $cash -gt $LOSS && $cash -lt $WIN ]]
+do
+	bet
+done
+}
