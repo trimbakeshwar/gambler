79ee1d0cf3565d9d4b66a0cc3914dc9bbca5dd4c
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-23 13:31:13 +0530

    Added:-to add daywise how much amount won or loss

--- gambler.sh
+++ gambler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 echo "welcome to gambler"
-declare -A record
+declare -A recordWon
 STAKE=100
 BET=10
 PERCENT=50
@@ -8,30 +8,39 @@ WIN=150
 LOSS=50
 day=2
 cash=$STAKE
+max=0
+min=0
+LukestDay=0
+badDay=0
 function bet(){
 result=$((RANDOM%2))
 if [ $result -eq 1 ]
 then
 	((cash+=$BET))
 	((amountWon+=$BET))
-	echo "$amountWon"
+#	echo "$amountWon"
 else
 	((cash-=$BET))
 	((amountLoss+=$BET))
-	echo "$amountLoss"
+#	echo "$amountLoss"
 fi
 }
 function playing(){
 for  (( i=1; i<=20; i++ ))
-do
+do	
+	amountWon=0
+	amountLoss=0
 	cash=$STAKE
 	while [[ $cash -ne $LOSS && $cash -ne $WIN ]]
 	do
 		bet
 	done
-done
 
+recordWon[$i]=$amountWon
+recordLoss[$i]=$amountLoss
+done
 }
 playing 
-echo "amount won : $amountWon"
-echo "amount lost : $amountLoss"
+
+echo "won ${!recordWon[@]}	${recordWon[@]}"
+echo "loss ${!recordLoss[@]} ${recordLoss[@]}"
