084424d4d6bb32f04b57b5fd81ea322d858d4fb1
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-28 18:02:49 +0530

    refactor:-change indentation and add comments

--- gambler.sh
+++ gambler.sh
@@ -8,45 +8,48 @@ WIN=150
 LOSS=50
 day=2
 cash=$STAKE
+# how much money use for bet
 function bet(){
-result=$((RANDOM%2))
-if [ $result -eq 1 ]
-then
-	((cash+=$BET))
-	((amountWon+=$BET))
-
-else
-	((cash-=$BET))
-	((amountLoss+=$BET))
-fi
+	result=$((RANDOM%2))
+	if [ $result -eq 1 ]
+	then
+		((cash+=$BET))
+		((amountWon+=$BET))
+	else
+		((cash-=$BET))
+		((amountLoss+=$BET))
+	fi
 }
+
+#play the game
+
 function playing(){
-max=0
-min=0
-LukestDay=0
-badDay=0
-for  (( i=1; i<=20; i++ ))
-do	
-	amountWon=0
-	amountLoss=0
-	cash=$STAKE
-	while [[ $cash -ne $LOSS && $cash -ne $WIN ]]
-	do
-		bet
+	max=0
+	min=0
+	LukestDay=0
+	badDay=0
+	for  (( i=1; i<=20; i++ ))
+	do	
+		amountWon=0
+		amountLoss=0
+		cash=$STAKE
+		while [[ $cash -ne $LOSS && $cash -ne $WIN ]]
+		do
+			bet
+		done
+		recordWon[$i]=$amountWon
+		recordLoss[$i]=$amountLoss
+		if [ $max -lt ${recordWon[$i]} ]
+		then
+   		max=${recordWon[$i]}
+   		LukestDay=$i
+		fi
+		if [ $min -lt ${recordLoss[$i]} ]
+		then
+   		min=${recordLoss[$i]}
+   		badDay=$i
+		fi
 	done
-recordWon[$i]=$amountWon
-recordLoss[$i]=$amountLoss
-if [ $max -lt ${recordWon[$i]} ]
-then
-   max=${recordWon[$i]}
-   LukestDay=$i
-fi
-if [ $min -lt ${recordLoss[$i]} ]
-then
-   min=${recordLoss[$i]}
-   badDay=$i
-fi
-done
 }
 
 while true
@@ -62,7 +65,7 @@ do
 		echo "max amount win on the day $LukestDay"
 		echo "max amount loss $min"
 		echo "max amount loss on the day $badDay"
-if [[ $choice == 0 ]]
+	if [[ $choice == 0 ]]
 	then
 		break
 	fi
